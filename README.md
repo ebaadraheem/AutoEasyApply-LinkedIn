2b1d9f0a638f0afebfe8419545524e13b7c04e66
Ebaad Raheem <ebaadraheem19@gmail.com>
2026-06-22 20:04:28 +0500

    Move frontend files into frontend directory

--- README.md
+++
@@ -1,6 +0,0 @@
-# Shopsphere_Frontend
-# Add .env file
-VITE_STRIPE_PUBLISHABLE_KEY=Your stripe publishable key
-VITE_BACKEND_URL=Your backend url
-VITE_ADMIN_ID=Your specific admin id
-VITE_FIREBASE_API_KEY=Your Firebase Api Key
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { Toaster } from 'react-hot-toast'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './index.css'
+import  { Toaster } from 'react-hot-toast';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Toaster/>
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+    <Toaster/>
+  </React.StrictMode>,
 )
